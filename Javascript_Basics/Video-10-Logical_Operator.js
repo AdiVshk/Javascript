05615c24dd5726e219c16a54ef0b450de205a61e
TheCoderBlast <adityavshk24@gmail.com>
2025-01-18 19:07:15 +0530

    Create Video-10-Logical_Operator.js

---
+++ Javascript_Basics/Video-10-Logical_Operator.js
@@ -0,0 +1,25 @@
+// Logical Operators
+
+// AND gate take two inputs
+// In AND gate if one value is False everything is false
+
+// OR gate take two inputs
+// In OR gate if one value is True everything is True
+
+// NOT gate takes only one input
+// X is true  --> false
+// Y is false --> true
+
+// If you want to write Logical AND in JS
+// &&
+
+// If you want to write Logical OR in JS
+// ||
+
+// If you want to write Logical NOT in JS
+// !value
+
+console.log(true && false); // false
+console.log(false || true); // true
+console.log(!false); // true
+console.log(10 > 5 && 900 < 100); // false
