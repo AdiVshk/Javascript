3f7588c44391436e0040d634cd35c659f57bc27f
TheCoderBlast <adityavshk24@gmail.com>
2025-01-18 18:20:45 +0530

    Create Video-7-Comments.js

---
+++ Javascript_Basics/Video-7-Comments.js
@@ -0,0 +1,15 @@
+// Comments 
+
+
+// Comments are for documentation purpose 
+// The programming language will just avoid comments during execution 
+
+
+// How do you create comments 
+
+// if you want to create line comments --> double slash (//)
+
+// multiple line comments --> /* */ 
+
+/* My name is Sanket Singh 2025 is going to be my Year for Sure !!!! */ 
+
