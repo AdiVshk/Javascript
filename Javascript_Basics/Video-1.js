81c3bda4e4e37dc77b18ab1ac1347165c8d64499
TheCoderBlast <adityavshk24@gmail.com>
2025-01-18 11:58:22 +0530

    Update Video-1.js

---
+++ Javascript_Basics/Video-1.js
@@ -0,0 +1,14 @@
+
+// In a  programming lang , few words are reserved by the lang for its own purpose we cannot change there meaning or repurpose them for a 
+// different use case 
+
+// for ex: let, for ,const ,if ,else, return etc 
+
+// How can we store some values inside a JS program 
+// to store values we can use variables 
+
+// Variables are memory bucket that store our values and has name as well 
+
+// Inside the variable we can store  the data 
+
+
