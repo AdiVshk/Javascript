c05bf078f6b54239d97eb42332e9dccc494a328d
TheCoderBlast <adityavshk24@gmail.com>
2025-01-18 18:48:51 +0530

    Create Video-9-Relational_Operators.js

---
+++ Javascript_Basics/Video-9-Relational_Operators.js
@@ -0,0 +1,20 @@
+// Relational Operators 
+
+// < , > , <= , >= 
+
+// operand1 and Operand2 
+
+//(10 < 12 ) --> return true and False based on the comparison 
+
+let x = 10;
+let y = 20;
+let z = 5;
+let a = 10;
+
+console.log("x < z", x<z); // less than
+console.log("y > z" , y>z); // greater than
+console.log("x<=z",x<=z);  // less than or equal to 
+console.log("x>=a",x>=a);  // greater than or equal to 
+
+
+
