c3df41c4eb732a4aff18149dc71447634dccfcf7
TheCoderBlast <adityavshk24@gmail.com>
2025-01-18 18:33:37 +0530

    Create Video-8-Operators_Arthmetic_and_Assignment.js

---
+++ Javascript_Basics/Video-8-Operators_Arthmetic_and_Assignment.js
@@ -0,0 +1,45 @@
+
+
+// Operators 
+
+// There are different types of Operators 
+
+
+// Arithmetic Operators 
+// + , - , / , * , % 
+
+// Where do you apply operators ? 
+// On operands- values which we want to do operation 
+
+let a = 30;
+let b = 40;
+
+// 30 and 40 --> are operands 
+// + --> is operator
+
+console.log(a+b); // addition 
+console.log(a-b); // subtraction 
+console.log(a*b); // multiplication
+console.log(a/b); // division --> Quotient
+console.log(a%b); // division --> remainder 
+console.log(a**3); // exponent 
+
+
+// Assignment Operators 
+
+// = , += , -= , /= , %= 
+
+
+let x = 10;
+// = --> assignment operator
+// just directly assigns the value from RHS to LHS 
+
+// x+=2 --> x=x+2 
+// x-=2 --> x=x-2
+// x/=2 --> x=x+2
+// x%=2 --> x=x%2 
+
+console.log(x+=2);
+console.log(x-=2);
+console.log(x/=2);
+console.log(x%=2);
