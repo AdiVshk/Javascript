b379a2e63fc75f2af9b49c1f06c74dcd2aba58a5
TheCoderBlast <adityavshk24@gmail.com>
2025-01-18 12:27:53 +0530

    Create Video-2-Coding_Variables_in_JS.js

---
+++ Javascript_Basics/Video-2-Coding_Variables_in_JS.js
@@ -0,0 +1,60 @@
+// File name of JS should be .js file
+
+// for example : video-1.js , Variable.js
+
+// How to create variables in JS
+
+// there are 3 ways to create variables in JS
+
+// first way - var keyword
+// Syntax
+// var <variable name> = <value>;
+var marks = 90;
+var score = 5;
+
+// second way - let keyword
+// let <variable name> = <value>;
+let age = 24;
+let flag = 0;
+
+// third way - const keyword
+// const <variable name> = <value>;
+const number = 10;
+const x = 900;
+
+
+// Semicolon is optional in JS 
+// It is not mandatory but in some languages it is mandatory like C++,JAVA 
+// semicolon is like full stop in english language 
+// It is recommended to put semicolon in JS (Best practise)
+
+
+// Naming Rules for variable creation
+
+// Variables can contain small alphabets , Capital alphabets , digits , underscore ( _ )  , $ 
+
+// We cannot have space or any other special character 
+// for ex : you cannot first space  as a variable name , # ,& 
+
+
+// We should give meaningful name so that reader can identify the names properly 
+
+// Variables name cannot start with digits but it can have digits in between or at last 
+
+// for ex: 
+// let 0james = 10; // Wrong Syntax 
+
+
+// We cannot use keywords as variable name 
+// for ex : let ,const ,return ,var , 
+
+
+// Invalid variables 
+
+// const first name = 10 //  Wrong Syntax 
+// const 3address = 32 //    Wrong Syntax  
+// const %name = 'Harikat'// Wrong Syntax  
+
+
+
+
