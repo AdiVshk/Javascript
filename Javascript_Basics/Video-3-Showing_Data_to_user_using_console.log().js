7c446703ad9e411cf22a215b7252798ebeb2b04c
TheCoderBlast <adityavshk24@gmail.com>
2025-01-18 12:45:06 +0530

    Create Video-3-Showing_Data_to_user_using_console.log().js

---
+++ Javascript_Basics/Video-3-Showing_Data_to_user_using_console.log().js
@@ -0,0 +1,28 @@
+// In-built functions
+// Javascript has many different in-built functions which can do different things  for you
+// You can create your own  functions but later in the course
+
+// console.log()
+// What is console.log()
+
+// this function takes some data as input and then displays them in the output
+
+// this function can take multiple arguments all together
+
+console.log(10); // 10
+console.log('Hello World'); // "Hello World"
+
+let age = 13;
+console.log(age);
+
+// its only task is to display on the screen
+
+// you can pass multiple values
+
+// if we have two different console.log then both of them displays / prints output in two  different line
+
+// we can also pass multiple values together to console.log() and all of them will be on the same line
+
+console.log(10, 'starting', 343, true);
+
+// you can pass comma seperated values that will give output in the same line
